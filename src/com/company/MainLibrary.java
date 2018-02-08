613877537a8332433c2f7628bf31e09fee1bf884
alex462 <alex.reinhart462@gmail.com>
2018-02-08 16:04:57 -0500

    created array for main library storage. working on addingGame method

--- src/com/company/MainLibrary.java
+++ src/com/company/MainLibrary.java
@@ -9,31 +9,39 @@ holds all games
 public class MainLibrary {
 
     Game game = new Game();
+    String mainLibraryStorage[] = new String[20];
 
     //method to add game to library
-    protected void addGame(){
+    protected void addGame() {
 
-        String mainLibraryStorage [] = new String [20];
-        Scanner input = new Scanner(System.in);
+
+        for (int g = 0; g <= 25; g++) {
+            System.out.println("Enter a game to add to your main library: ");
+            Scanner input = new Scanner(System.in);
+            mainLibraryStorage[g] = input.nextLine();
+            //add verification. if/then.
+            System.out.println("You have added " + game.getGameTitle() + " to your main library."); //confirmation
+            viewMainLibrary();
+        }
 
         //code to add game to main library
     }
 
     //method to remove game from main library
-    protected void removeGame(){
+    protected void removeGame() {
 
         //code to remove game from main library
     }
 
     //method to view main library
-    protected void viewMainLibrary(){
+    protected void viewMainLibrary() {
 
         //code to view main library
     }
 
     //method to store games in main library
-    protected void mainLibraryGames(){
+    protected void mainLibraryGames() {
 
-
+//        mainMenu.startMenu(); //extend MainLibrary from MainMenu class?
     }
 }
