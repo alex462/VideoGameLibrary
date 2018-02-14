604a0dbae7ead2f44e87ad1a3f418c3abdb3f5c3
alex462 <alex.reinhart462@gmail.com>
2018-02-14 09:09:32 -0500

    added removeGame method

--- src/com/company/MainLibrary.java
+++ src/com/company/MainLibrary.java
@@ -122,15 +122,68 @@ public class MainLibrary {
     }
 
     //method to remove game from main library
-    protected void removeGame() {
+    public void removeGame() {
 
-        //code to remove game from main library
+        if(mainLibArrayList.size() > 0) { //if/else stmt prevents method from running if there are no games in library to remove
+
+        isValidInput = false;
+        while (!isValidInput) {
+
+            System.out.println("MAIN LIBRARY"); //output list of games in main library for user to select from
+
+            int position = 1;
+            for (int i = 0; i < mainLibArrayList.size(); i++) {
+                System.out.println(position + ". " + mainLibArrayList.get(i));
+                position++;
+            }
+            System.out.println("Enter a game title to remove from your main library: ");
+
+            Game game = new Game(input.nextLine());
+//            game.setGameTitle();
+
+            System.out.println("Are you sure you want to add " + game.getGameTitle() + " to your main library?");
+            System.out.println("1. Yes\n2. No; return to main menu\n3. Exit program");
+
+            switch (input.nextLine()) {
+
+                case "1": //yes
+                    System.out.println("You have removed " + game.getGameTitle() + " from your main library.");
+                    mainLibArrayList.add(game);
+                    removeAnotherGame();
+                    isValidInput = true;
+                    break;
+
+                case "2": //no; main menu
+                    mainMenu.startMenu();
+                    isValidInput = true;
+                    break;
+
+                case "3": //exit
+                    exitProgram();
+                    isValidInput = true;
+                    break;
+
+                default: //incorrect user input - prompt user for correct number, loop back to MainMenu
+                    invalidEntry();
+                    isValidInput = false;
+            }
+        }
+
+            }else{
+            System.out.print("There are no games in your main library at this time.");
+            mainMenu.startMenu();
+        }
+
+    }
+
+    public void removeAnotherGame(){
+
     }
 
     //method to view main library
-    protected void viewMainLibrary() {
-        System.out.println("MAIN LIBRARY");
+    public void viewMainLibrary() {
 
+        System.out.println("MAIN LIBRARY");
 
         int position = 1;
         for (int i = 0; i < mainLibArrayList.size(); i++) {
@@ -153,7 +206,9 @@ public class MainLibrary {
     //method to add game to library
     public void checkInGame() {
 
-        isValidInput = false;
+        if(checkoutLibArrayList.size() > 0) { //if/else stmt prevents method from running if there are no games in checkout to remove
+
+            isValidInput = false;
         while (!isValidInput) {
 
             System.out.println("Enter a game title to check in to your main library: ");
@@ -188,6 +243,11 @@ public class MainLibrary {
                     invalidEntry();
                     isValidInput = false;
             }
+        }
+
+        }else{
+            System.out.print("You have no games checked out at this time.");
+            mainMenu.startMenu();
         }
     }
 
@@ -224,7 +284,7 @@ public class MainLibrary {
     }
 
     //method to remove game from checkout library
-    protected void checkOutGame() {
+    public void checkOutGame() {
 
         isValidInput = false;
         while (!isValidInput) {
@@ -243,6 +303,7 @@ public class MainLibrary {
                     System.out.println("You have checked out " + game.getGameTitle() + " from your main library.");
                     mainLibArrayList.remove(game);
                     checkoutLibArrayList.add(game);
+                    checkoutAnotherGame();
                     isValidInput = true;
                     break;
 
@@ -264,8 +325,12 @@ public class MainLibrary {
         }
     }
 
+    public void checkoutAnotherGame(){
+
+    }
+
     //method to view checkout library
-    protected void viewCheckoutLibrary() {
+    public void viewCheckoutLibrary() {
 
         System.out.println("CHECKED-OUT VIDEO GAMES");
 
